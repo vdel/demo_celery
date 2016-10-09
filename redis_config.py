ca3a52b823536dfc1f1a36c7658e6926f14da096
Vincent Delaitre <vincent.delaitre@gmail.com>
2016-10-09 10:32:07 +0200

    fix

--- redis_config.py
+++ redis_config.py
@@ -1,5 +1,5 @@
 REDIS_PASSWORD = '1234'
-BROKER_URL = 'redis://:%s@localhost:6379/%%d' % REDIS_PASSWORD
+BROKER_URL = 'redis://:%s@localhost:6379/%d'
 
 def get_broker_url(db_id):
-    return BROKER_URL % db_id
+    return BROKER_URL % (REDIS_PASSWORD, db_id)
